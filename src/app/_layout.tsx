0053dae849dede804f818f48a6ef56e7c4fc2646
warit.ng@ku.th <warit.ng@ku.th>
2026-09-08 12:49:04 +0700

    123

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
 import { Stack } from 'expo-router';
+
 export default function Layout() {
-  // ปิดหัว Header ของ Expo ทิ้ง
+  // ปิดหัว Header ของ Expo ทิ้ง เพื่อให้ Header ธีม Steam ของเราแสดงผลแทน
   return <Stack screenOptions={{ headerShown: false }} />;
 }
-=======
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
-}
->>>>>>> 0a8581912baf9d764f7f5dd593beff601fbe8ae2
